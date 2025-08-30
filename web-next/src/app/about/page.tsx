c26b1d74b7219fd801d3e8988e1f7d2bf561b9fe
Nattakit-Kongtan <96936356+Nattakit-Kongtan@users.noreply.github.com>
2025-08-30 23:43:42 +0700

    add API path

---
+++ web-next/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Navbar from "@/components/navbar";
+
+export default function About() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Navbar />
+
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-3xl font-bold">About</h1>
+        <p>เกี่ยวกับฉัน</p>
+      </main>
+    </div>
+  );
+}
---
+++ web-next/src/components/navbar.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image"
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="fixed top-0 left-0 w-full bg-blue-500 text-black font-weight:bold px-4 py-3 flex justify-between items-center shadow-md z-50">
+      {/* Logo */}
+      <div className="text-lg font-bold">
+        <Link href="/">
+            <Image 
+            src="/globe.svg"    
+            alt="MyApp Logo"
+            width={120}
+            height={40}
+            priority
+            />
+        </Link>
+        </div>
+
+      <button
+        className="md:hidden bg-blue-500 px-3 py-2 rounded"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        ☰
+      </button>
+
+      {/* ลิงก์เมนู */}
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } md:flex space-x-4 absolute md:static top-14 left-0 w-full md:w-auto bg-blue-600 md:bg-transparent p-4 md:p-0`}
+      ><Link href="/" className="block md:inline-block hover:underline text-black font-bold">
+        Home
+        </Link>
+        <Link href="/about" className="block md:inline-block hover:underline text-black font-bold">
+        About
+        </Link>
+        <Link href="/contact" className="block md:inline-block hover:underline text-black font-bold">
+        Contact
+        </Link>
+      </div>
+    </nav>
+  );
+}
